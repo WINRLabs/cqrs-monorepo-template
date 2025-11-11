7e0588fef933f92c6bc102cbd588245d7291c16a
buroz <hey@buroz.dev>
2025-11-11 19:36:56 +0300

    middleware fix

--- apps/auth/src/ratelimiter/middleware.ts
+++ apps/auth/src/ratelimiter/middleware.ts
@@ -10,25 +10,19 @@ const headers = ["CF-Connecting-IP", "X-Forwarded-For", "X-Real-IP"];
 const whiteListedIpBlocks = [new Netmask("127.0.0.1/32")];
 
 const getClientIp = (c: Context): string | null => {
-  if (process.env.NODE_ENV === "test") {
-    return "127.0.0.1";
-  }
+  const remoteAddress = c.env.incoming.socket.remoteAddress;
+
+  const isInternal = [
+    process.env.NODE_ENV === "test",
+    remoteAddress === "::1",
+    whiteListedIpBlocks.some((ip) => ip.contains(remoteAddress)),
+  ].some((condition) => condition);
+
+  if (isInternal) return "127.0.0.1";
 
   for (const header of headers) {
     const value = c.req.header(header);
-    if (value) {
-      return value;
-    }
-  }
-
-  const remoteAddress = c.env.incoming.socket.remoteAddress;
-
-  if (remoteAddress === "::1") {
-    return "127.0.0.1";
-  }
-
-  if (whiteListedIpBlocks.some((ip) => ip.contains(remoteAddress))) {
-    return "127.0.0.1";
+    if (value) return value;
   }
 
   return remoteAddress;
